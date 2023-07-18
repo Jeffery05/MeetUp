ab6697fbd2c7c40cfc2dfbc28f4239122634a669
Jeffery Hu <jefferyhu2005@gmail.com>
2023-07-17 23:18:56 -0400

    added the code for creating a new user

--- website/__init__.py
+++ website/__init__.py
@@ -16,11 +16,3 @@ from .auth import auth
 
 app.register_blueprint(views, url_prefix='/') # URL prefix goes in front of whatever is in route
 app.register_blueprint(auth, url_prefix='/')
-
-"""
-def create_database(app):
-    if not path.exists('website/' + DB_NAME): # checks if database exists
-        db.create_all(app=app) # if it doesn't we create it
-        print('Create Database!')
-        """
-        
