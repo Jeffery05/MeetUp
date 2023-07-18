78c1588f1952145891c665c72c4ac28090780029
Jeffery Hu <jefferyhu2005@gmail.com>
2023-07-18 11:39:05 -0400

    implemented the flask login module

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -21,3 +22,14 @@ def create_database(app):
     if not path.exists('website/' + DB_NAME):
         with app.app_context():
             db.create_all()
+
+
+from .models import User
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- website/views.py
+++ website/views.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__) # define blueprint for our application
 
 @views.route('/') #decorator: whenever you go to the / URL, whatever in hom() will run
+@login_required
 def home():
     return render_template("home.html")
