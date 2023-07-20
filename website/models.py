c969d189ea1aed8ce7e59687d4c05d8be5bb3b88
Jeffery Hu <jefferyhu2005@gmail.com>
2023-07-19 23:36:30 -0400

    added association table

--- website/models.py
+++ website/models.py
@@ -2,6 +2,20 @@ from . import db
 from flask_login import UserMixin 
 from sqlalchemy.sql import func
 
+"""
+user_meetup = db.Table('user_meetup', 
+                       db.Column('user_id', db.Integer, db.ForeignKey('user.id')), 
+                       db.Column('meetup_id', db.Integer, db.ForeignKey('meetup.id')))
+"""
+# Note: the "User" class is referenced in sql in lowercase (i.e. "user")
+           
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True) # primary key = unique identifier to represent the object
+    email = db.Column(db.String(150), unique = True) # no user can have the same email as someone else
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    notes = db.relationship('Note', backref="user") # tells SQL that whenever we create a note, add the note id into this user's notes relationship (creates a list of all the notes by the user)
+
 # database model: blueprint for how each object will look like
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,11 +23,3 @@ class Note(db.Model):
     date = db.Column(db.DateTime(timezone=True), default=func.now()) # gets current date and time, store it as date
     user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # foreign key links user to the note
 
-# Note: the "User" class is referenced in sql in lowercase (i.e. "user")
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True) # primary key = unique identifier to represent the object
-    email = db.Column(db.String(150), unique = True) # no user can have the same email as someone else
-    password = db.Column(db.String(150))
-    first_name = db.Column(db.String(150))
-    notes = db.relationship('Note', backref="user") # tells SQL that whenever we create a note, add the note id into this user's notes relationship (creates a list of all the notes by the user)
-            
